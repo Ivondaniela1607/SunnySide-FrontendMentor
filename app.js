48fb1eabcf1a71a8dae35c6c073d6070c240c965
Daniela <ivondaniela1607@gmail.com>
2022-08-17 08:07:27 -0500

    ajustes issues

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 document.getElementById("btn_menu").addEventListener("click", mostrar_menu)
-document.getElementById("btn_opt").addEventListener("click", ocultar_menu)
 
 nav = document.getElementById("nav");
-back_menu = document.getElementById("back_menu")
-
 
 function mostrar_menu() {
-  console.log("holii")
   nav.style.display = "block";
-}
-
-function ocultar_menu() {
-  nav.style.display = "none";
-  
 }
